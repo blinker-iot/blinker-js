3b2a9dcc497ded0b040f4826511dd1b7e2dd5b0c
clz <coloz999@gmail.com>
2020-06-24 09:38:14 +0800

    更新

--- blinker-cli.ts
+++ blinker-cli.ts
@@ -6,6 +6,27 @@ import mqtt from 'mqtt';
 const API_Register = "";
 const API_RegisterCheck = ""
 
+
+const { program } = require('commander');
+
+program
+    .option('--no-sauce', 'Remove sauce')
+    .option('--cheese <flavour>', 'cheese flavour', 'mozzarella')
+    .option('--no-cheese', 'plain with no cheese')
+
+
+program
+    .command('start <service>', 'start named service')
+    .command('stop [service]', 'stop named service, or all if no name supplied')
+    .parse(process.argv);
+
+
+program.on('--help', () => {
+    console.log('');
+    console.log('Example call:');
+    console.log('  $ custom-help --help');
+});
+
 function createConfig() {
 
 }
